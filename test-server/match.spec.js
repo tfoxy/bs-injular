c3d0d5eef36452981602a8962cc050846d6b6513
Tomás Fox <tomas.c.fox@gmail.com>
2016-07-11 14:19:05 -0300

    Improve match

--- test-server/match.spec.js
+++ test-server/match.spec.js
@@ -10,5 +10,16 @@ describe('match', () => {
     let matched = match('foo/foo/bar', ['qwerty', 'foo/*/bar', 'azerty']);
     expect(matched).to.equal(true);
   });
+  
+  it('should work when providing an array of patterns with negative matches', () => {
+    let matched;
+    let patterns = ['**/*.js', '!foo/*.js', 'foo/bar.js'];
+    matched = match('bar/foo.js', patterns);
+    expect(matched).to.equal(true, 'path: bar/foo.js');
+    matched = match('foo/foo.js', patterns);
+    expect(matched).to.equal(false, 'path: foo/foo.js');
+    matched = match('foo/bar.js', patterns);
+    expect(matched).to.equal(true, 'path: foo/bar.js');
+  });
 
 });
